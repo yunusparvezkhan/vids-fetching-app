96d049448c6065b59eb9b5cf792f6620a28c4df5
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-08 17:54:01 +0530

    disabled form's default behaviour on form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,15 @@ import "semantic-ui-css/semantic.min.css"
 export default class SearchBar extends Component {
     state = { query: "" }
 
+    onSearchSubmit = event => {
+        event.preventDefault();
+        // send search query to parent
+    }
+
     render() {
         return (
             <div className='ui segment search-bar'>
-                <form className='ui form'>
+                <form onSubmit={this.onSearchSubmit} className='ui form'>
                     <div className='field'>
                         <label>Search Video</label>
                         <input type="text" onChange={(input) => this.setState({ query: input.target.value })} value={this.state.query} />
