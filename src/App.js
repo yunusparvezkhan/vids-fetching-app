11e992c27e665323ef07c5477161fbf3171cfb86
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-08 17:37:23 +0530

    added a little style to main div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import "semantic-ui-css/semantic.min.css"
 import SearchBar from "./components/SearchBar";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container ui'>
         <SearchBar />
       </div>
     )
