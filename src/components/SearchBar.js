e31ed30b2ac4bbd8ac8750f8561233a5e08de8c4
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-08 17:35:30 +0530

    Fix to previous commit, Relinked Semantic UI
    This time linked semantic ui through their npm package.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "semantic-ui-css/semantic.min.css"
 
 export default class SearchBar extends Component {
     render() {
         return (
-            <div>SearchBar</div>
+            <div className='ui segment search-bar'>
+                <form className='ui form'>
+                    <div className='field'>
+                        <label>Search Video</label>
+                        <input type="text" />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
