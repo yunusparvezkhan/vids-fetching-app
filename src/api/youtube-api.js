406ee5ad167b847ad5cbae77bd95198d02fb75c5
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-10 16:17:17 +0530

    Added api params and configuration

--- src/api/youtube-api.js
+++ src/api/youtube-api.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 const KEY = AIzaSyBQtlfdd5xkBn4WX3md22D_DMtZDipey6Y;
+
+export default axios.create({
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+        part: "snippet",
+        maxResults: 5,
+        key: KEY
+    }
+})
