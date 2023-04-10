2189d4b534e6f1d08f6629c1d48cf1eddf1aeb7f
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-10 22:46:12 +0530

    created VideoList and VideoItem components and linked according to the hairarchy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import "semantic-ui-css/semantic.min.css"
-import youtube from "./api/youtube-api";
 
 import SearchBar from "./components/SearchBar";
+import youtube from "./api/youtube-api";
+import VideoList from "./components/VideoList";
 
 
 export default class App extends Component {
@@ -24,6 +25,8 @@ export default class App extends Component {
     return (
       <div className='container ui'>
         <SearchBar onQuerySubmit={this.onQuerySubmit} />
+        <span>Got {this.state.videos.length} videos</span>
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
---
+++ src/components/VideoItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoItem = (props) => {
+    return (
+        <div>{props.video}</div>
+    )
+}
+
+export default VideoItem
